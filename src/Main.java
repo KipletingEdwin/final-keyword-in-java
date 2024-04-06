367b0b5f8383f275e836eb7dcdc0d6a3f97b5c9d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-06 22:15:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        final double PI = 3.14159;
+        //cannot change the value
+
+        System.out.println(PI);
+
+    }
+}
